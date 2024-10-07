2e5a0f903672a71d9087f6f18448359092c6dda9
Elsera05 <achmadrasyid53@gmail.com>
2024-10-07 09:06:21 +0700

    update revisi backend course

--- prisma/migrations/20241004044031_init/migration.sql
+++ prisma/migrations/20241004044031_init/migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE "cars" (
     "seat" INTEGER,
     "baggage" INTEGER,
     "transmission" VARCHAR,
-    "year" DATE,
+    "year" VARCHAR,
     "name" VARCHAR,
     "description" TEXT,
     "isDriver" BOOLEAN,
@@ -15,7 +15,7 @@ CREATE TABLE "cars" (
     "img" TEXT,
     "price" INTEGER,
     "createdDt" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
-    "updatedDt" TIMESTAMP(6),
+    "updatedDt" TIMESTAMP(3),
     "createdBy" VARCHAR,
     "updatedBy" VARCHAR,
 
@@ -23,31 +23,28 @@ CREATE TABLE "cars" (
 );
 
 -- CreateTable
-CREATE TABLE "districts" (
-    "kode_district" INTEGER,
-    "kode_regencies" INTEGER,
-    "nama" VARCHAR(50)
-);
-
--- CreateTable
 CREATE TABLE "order" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "order_no" VARCHAR NOT NULL,
-    "user_id" BIGINT NOT NULL,
-    "car_id" BIGINT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "car_id" INTEGER NOT NULL,
     "start_time" TIMESTAMP(6),
     "end_time" TIMESTAMP(6),
     "total" DOUBLE PRECISION,
     "is_driver" BOOLEAN,
     "is_expired" BOOLEAN,
     "status" VARCHAR,
+    "createdDt" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updatedDt" TIMESTAMP(3),
+    "createdBy" VARCHAR,
+    "updatedBy" VARCHAR,
 
     CONSTRAINT "order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "users" (
-    "id" BIGSERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "fullname" VARCHAR NOT NULL,
     "email" VARCHAR(30) NOT NULL,
     "password" VARCHAR NOT NULL,
@@ -58,8 +55,8 @@ CREATE TABLE "users" (
     "phone_number" VARCHAR NOT NULL,
     "driver_license" TEXT,
     "birthdate" DATE,
-    "createdDt" TIMESTAMP(6),
-    "updatedDt" TIMESTAMP(6),
+    "createdDt" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updatedDt" TIMESTAMP(3),
     "createdBy" VARCHAR,
     "updatedBy" VARCHAR,
 
@@ -70,9 +67,6 @@ CREATE TABLE "users" (
 CREATE INDEX "car_name_index" ON "cars"("name");
 
 -- CreateIndex
-CREATE INDEX "idx_district_name" ON "districts"("nama");
-
--- CreateIndex
 CREATE UNIQUE INDEX "order_order_no_key" ON "order"("order_no");
 
 -- CreateIndex
@@ -80,3 +74,9 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_phone_number_key" ON "users"("phone_number");
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "cars"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
