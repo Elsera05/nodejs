0d9bcef1c0246d77c392065219d845db92ecd080
Elsera05 <achmadrasyid53@gmail.com>
2024-10-16 14:57:00 +0700

    testing reb

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const server = require ("../index");
+const server = require("../index");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
 
@@ -8,11 +8,49 @@ const testUser = {
     password: "Password123!",
 };
 
-describe("POST /api/v1/auth/signUp",()=> {
-    it ("should response with 200 status code",()=>{
+describe("POST /api/v1/auth/signup", () => {
+    it("should response with 200 status code", () => {
+        request(server)
+            .post("api/v1/auth/signup")
+            .send(testUser)
+            .set("Accept", "application/json")
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body).objectContaining({
+                    code: 200,
+                    status: "success",
+                    message: "Sign up succesfully",
+                    data: expect.objectContaining({
+                        user: {
+                            email: "test@test.com",
+                            password: expect.not.stringContaining("Password123!"),
+                            address: null,
+                            avatar: null,
+                            birthdate: null,
+                            fullname: null,
+                            driver_license: null,
+                            gender: null,
+                            phone_number: null,
+                            roleID: 3,
+                            createBy: null,
+                            createDt: expect(
+                                new Date(res.body.data.user.createDt))
+                                .toBeInstaceOf(Date),
+                            updateBy: null,
+                            updateDT: expect(
+                                new Date(res.body.data.user.updateDt))
+                                .toBeInstaceOf(Date),
+                        }
+                    })
+                })
+            })
+    })
+    it("should response with 400 status code", () => {
 
     })
-    it ("should response with 400 status code",()=>{
-        
-    })
 })
+
+// afterAll(() => {
+//     prisma.users.deleteMany();
+//     server.close()
+// })
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    setupFilesAfterEnv: ["__test__/setup.js"],
+    testEnvirontment: 'node',
+}
